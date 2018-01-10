98518627c9f41ccbcd6331c01fbb164a3ef76d2e
B.S.Lee <254608684@qq.com>
2018-01-10 16:25:21 +0800

    update

--- recyclerlib/src/main/java/com/supets/pet/MainTabActivity.java
+++ recyclerlib/src/main/java/com/supets/pet/MainTabActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -33,58 +34,84 @@ public class MainTabActivity extends AppCompatActivity {
 
     private class MainTabAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-        private ArrayList<String> data = new ArrayList<>();
+        private ArrayList<ClickType> data = new ArrayList<>();
 
         public MainTabAdapter() {
-            data.add("RecyclerView实现悬浮");
-            data.add("RecyclerView实现Gallery");
-            data.add("RecyclerView实现Banner");
-            data.add("RecyclerView实现下拉刷新和上拉加载更多");
+            data.add(new ClickType(0, "RecyclerView实现悬浮"));
+            data.add(new ClickType(1, "RecyclerView实现Gallery"));
+            data.add(new ClickType(2, "RecyclerView实现Banner"));
+            data.add(new ClickType(3, "RecyclerView实现下拉刷新和上拉加载更多"));
         }
 
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new RecyclerView.ViewHolder(new TextView(parent.getContext())) {
+            return new RecyclerView.ViewHolder(new MainViewHolder(parent).mWholeView) {
             };
         }
 
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
-            TextView name = (TextView) holder.itemView;
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    qq(view, position);
-                }
-            });
-            name.setText(data.get(position));
+            MainViewHolder mainViewHolder = (MainViewHolder) holder.itemView.getTag();
+            mainViewHolder.updateData(data.get(position));
         }
 
-        private void qq(View view, int position) {
-            if (position == 0) {
-                Intent intent = new Intent(view.getContext(), SuspendActivity.class);
-                view.getContext().startActivity(intent);
-            }
-
-            if (position == 1) {
-                Intent intent = new Intent(view.getContext(), GalleryActivity.class);
-                view.getContext().startActivity(intent);
-            }
-
-            if (position == 2) {
-                Intent intent = new Intent(view.getContext(), BannerMainActivity.class);
-                view.getContext().startActivity(intent);
-            }
-
-            if (position == 3) {
-                Intent intent = new Intent(view.getContext(), LoadMoreWrapperActivity.class);
-                view.getContext().startActivity(intent);
-            }
-        }
 
         @Override
         public int getItemCount() {
             return data.size();
+        }
+
+        public class MainViewHolder implements View.OnClickListener {
+
+            private View mWholeView;
+            private TextView name;
+            private ClickType mData;
+
+            public MainViewHolder(ViewGroup parent) {
+                mWholeView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_main, parent, false);
+                name = mWholeView.findViewById(R.id.name);
+                mWholeView.setOnClickListener(this);
+                mWholeView.setTag(this);
+            }
+
+            public void updateData(ClickType data) {
+                this.mData = data;
+                name.setText(mData.name);
+            }
+
+            @Override
+            public void onClick(View view) {
+                if (mData.type == 0) {
+                    Intent intent = new Intent(view.getContext(), SuspendActivity.class);
+                    view.getContext().startActivity(intent);
+                }
+
+                if (mData.type == 1) {
+                    Intent intent = new Intent(view.getContext(), GalleryActivity.class);
+                    view.getContext().startActivity(intent);
+                }
+
+                if (mData.type == 2) {
+                    Intent intent = new Intent(view.getContext(), BannerMainActivity.class);
+                    view.getContext().startActivity(intent);
+                }
+
+                if (mData.type == 3) {
+                    Intent intent = new Intent(view.getContext(), LoadMoreWrapperActivity.class);
+                    view.getContext().startActivity(intent);
+                }
+            }
+        }
+
+
+        public class ClickType {
+            public int type;
+            public String name;
+
+            public ClickType(int type, String name) {
+                this.type = type;
+                this.name = name;
+            }
         }
     }
 
