39f337308f53467ae435d7eea3fb9172dd593075
B.S.Lee <254608684@qq.com>
2018-01-17 11:06:46 +0800

    update

--- app/src/main/java/com/supets/pet/zxing/TestGeneratectivity.java
+++ app/src/main/java/com/supets/pet/zxing/TestGeneratectivity.java
@@ -192,24 +192,11 @@ public class TestGeneratectivity extends AppCompatActivity {
 
     @SuppressLint("StaticFieldLeak")
     private void decode(final Bitmap bitmap, final String errorTip) {
-        /*
-        这里为了偷懒，就没有处理匿名 AsyncTask 内部类导致 Activity 泄漏的问题
-        请开发在使用时自行处理匿名内部类导致Activity内存泄漏的问题，处理方式可参考 https://github.com/GeniusVJR/LearningNotes/blob/master/Part1/Android/Android%E5%86%85%E5%AD%98%E6%B3%84%E6%BC%8F%E6%80%BB%E7%BB%93.md
-         */
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... params) {
-                return QRCodeDecoder.syncDecodeQRCode(bitmap);
-            }
-
-            @Override
-            protected void onPostExecute(String result) {
-                if (TextUtils.isEmpty(result)) {
-                    Toast.makeText(TestGeneratectivity.this, errorTip, Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(TestGeneratectivity.this, result, Toast.LENGTH_SHORT).show();
-                }
-            }
-        }.execute();
+        String result=QRCodeDecoder.syncDecodeQRCode(bitmap);
+        if (TextUtils.isEmpty(result)) {
+            Toast.makeText(TestGeneratectivity.this, errorTip, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(TestGeneratectivity.this, result, Toast.LENGTH_SHORT).show();
+        }
     }
 }
