37e0871f8ee1db6add3a4b527f2edd6c3d1d751d
B.S.Lee <254608684@qq.com>
2018-01-31 11:31:57 +0800

    update

--- mocklib/src/main/java/com/supets/pet/mocklib/MockConfig.java
+++
@@ -1,9 +0,0 @@
-package com.supets.pet.mocklib;
-
-public class MockConfig {
-
-    public static boolean getDebugMode() {
-        return true;
-    }
-    
-}
--- mocklib/src/main/java/com/supets/pet/mocklib/core/IMockService.java
+++
@@ -1,7 +0,0 @@
-package com.supets.pet.mocklib.core;
-
-
-public interface IMockService  {
-    boolean getMapper(String url);
-    String  getData(String url);
-}
